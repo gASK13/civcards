7ca556932ae61c5791eae7a22a4e9b7e210832b4
gASK13 <32205999+gASK13@users.noreply.github.com>
2017-09-22 23:07:24 +0200

    Update cards.js

--- cards.js
+++ cards.js
@@ -4,12 +4,15 @@
     if (type == 'Warrior') {
       card.name = 'Warrior';
       card.type = 'UNIT';
-      card.cost = 1;
+      card.cost = 2;
     } else if (type == 'Farm') {
       card.name = 'Farm';
       card.type = 'BUILDING';
       card.subtype = 'PRODUCTION';
       card.cost = 3;
+      card.effect = function(player) { 
+         player.gold++;
+      };
     } else if (type == 'Archer') {
       card.name = 'Archer';
       card.type = 'UNIT';
@@ -19,6 +22,19 @@
       card.type = 'BUILDING';
       card.subtype = 'CITY';
       card.cost = 0;
+    } else if (type == 'Call to Arms') {
+      card.name = 'Call to Arms';
+      card.type = 'EVENT';
+      card.cost = 1;
+      card.effect = function(player) { 
+        player.deck.push(createCard('Recruit', player.player));
+        player.deck.push(createCard('Recruit', player.player));
+        player.deck.push(createCard('Recruit', player.player));
+      };
+    } else if (type == 'Recruit') {
+      card.name = 'Recruit';
+      card.type = 'UNIT';
+      card.cost = 1;
     }
     
     card.owner = owner;
