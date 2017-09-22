d0b24d4e5a4720cda5316f6cd95b06277a38ff10
gASK13 <32205999+gASK13@users.noreply.github.com>
2017-09-22 21:39:51 +0200

    Create board.js

---
+++ board.js
@@ -0,0 +1,33 @@
+function createBoard() {
+  var board = Create2DArray(9);
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 3; j++) {
+      board[i,j] = new {};
+      if (i < 3) {
+        board[i,j].build = 'PLAYER';
+      }
+      if (i < 4) {
+        board[i,j].units = 'PLAYER';
+      }
+      if (i >= 6) {
+        board[i,j].build = 'ENEMY';
+      } 
+      if (i >=5) {
+        board[i,j].units = 'ENEMY';
+      }
+    }
+   }
+   
+   board[1,1].card = createCard('Village', 'PLAYER');
+   board[1,7].card = createCard('Village', 'ENEMY');
+};
+
+function Create2DArray(rows) {
+  var arr = [];
+
+  for (var i=0;i<rows;i++) {
+     arr[i] = [];
+  }
+
+  return arr;
+};
