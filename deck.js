64de740c8e448a07b1f541cf3bc1712d9c23d0d6
gASK13 <32205999+gASK13@users.noreply.github.com>
2017-09-22 22:28:37 +0200

    Update deck.js

--- deck.js
+++ deck.js
@@ -1,14 +1,14 @@
 // createDeck
-  function createDeck() {
+  function createDeck(owner) {
     var deck = [];
     for (i = 0; i < 10; i++) {
-      deck.push(createCard('Warrior'));
+      deck.push(createCard('Warrior', owner));
     }
     for (i = 0; i < 5; i++) {
-      deck.push(createCard('Archer'));
+      deck.push(createCard('Archer', owner));
     }
     for (i = 0; i < 3; i++) {
-      deck.push(createCard('Farm'));
+      deck.push(createCard('Farm', owner));
     }
     return deck;
   };
