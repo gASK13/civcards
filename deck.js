6ff2057f522dca8051152d80f4709426486dedff
gASK13 <32205999+gASK13@users.noreply.github.com>
2017-09-22 21:28:44 +0200

    Create deck.js

---
+++ deck.js
@@ -0,0 +1,32 @@
+// createDeck
+  function createDeck() {
+    var deck = [];
+    for (i = 0; i < 10; i++) {
+      deck.push(createCard('Warrior'));
+    }
+    for (i = 0; i < 5; i++) {
+      deck.push(createCard('Archer'));
+    }
+    for (i = 0; i < 3; i++) {
+      deck.push(createCard('Farm'));
+    }
+    return deck;
+  };
+
+  Array.prototype.move = function(from, to) {
+    this.splice(to, 0, this.splice(from, 1)[0]);
+  };
+  
+  // shuffle deck
+  function shuffleDeck(deck) {
+    for(i = 0; i < deck.length; i++) {
+      deck.move(i, Math.floor(Math.random() * deck.length));
+    }
+    return deck;
+  };
+ 
+ function drawCard(deck, hand) {
+    hand.push(deck.pop);
+ };
+  
+  
